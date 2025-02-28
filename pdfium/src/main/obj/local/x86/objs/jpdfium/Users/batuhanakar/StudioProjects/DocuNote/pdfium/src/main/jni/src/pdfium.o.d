91319f6ab08771849cb8e5158df793424528a28f
Batuhan <batuhanakr.akar@gmail.com>
2025-03-01 01:02:01 +0300

    v1

---
+++ pdfium/src/main/obj/local/x86/objs/jpdfium/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/pdfium.o.d
@@ -0,0 +1,18 @@
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/obj/local/x86/objs/jpdfium//Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/pdfium.o: \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/pdfium.cpp \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/util.hpp \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdfview.h \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_doc.h \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_annot.h \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_formfill.h \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_edit.h \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_save.h \
+  /Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_text.h
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/util.hpp:
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdfview.h:
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_doc.h:
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_annot.h:
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_formfill.h:
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_edit.h:
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_save.h:
+/Users/batuhanakar/StudioProjects/DocuNote/pdfium/src/main/jni/src/../include/fpdf_text.h:
